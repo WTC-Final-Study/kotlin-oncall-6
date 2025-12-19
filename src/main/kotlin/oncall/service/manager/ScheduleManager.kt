b1c57633b5ee53d434bc0455f375a854cb6c7964
joon0447 <joon0447@naver.com>
2025-12-19 14:53:26 +0900

    refactor(manager): 함수 분리

--- src/main/kotlin/oncall/service/manager/ScheduleManager.kt
+++ src/main/kotlin/oncall/service/manager/ScheduleManager.kt
@@ -6,48 +6,59 @@ import oncall.model.WorkType
 object ScheduleManager {
 
     fun generateSchedule(calendar: List<Day>, workTurn: Map<WorkType, List<String>>): List<Day> {
-        val schedule = mutableListOf<Day>()
         val weekdayTurn = workTurn[WorkType.WEEKDAY]!!.toMutableList()
         val holidayTurn = workTurn[WorkType.HOLIDAY]!!.toMutableList()
         var currentWeekdayIndex = 0
         var currentHolidayIndex = 0
         var yesterdayWorker = ""
-        calendar.forEach { day ->
-            var scheduleDay = day
-            val type = if (day.isHoliday) WorkType.HOLIDAY else WorkType.WEEKDAY
-            when (type) {
-                WorkType.WEEKDAY -> {
-                    var behind = 1
-                    val size = weekdayTurn.size
-                    while (yesterdayWorker == weekdayTurn[currentWeekdayIndex % size]) {
-                        val tmp = weekdayTurn[currentWeekdayIndex % size]
-                        weekdayTurn[currentWeekdayIndex % size] = weekdayTurn[(currentWeekdayIndex + behind) % size]
-                        weekdayTurn[(currentWeekdayIndex + behind) % size] = tmp
-                        behind++
-                    }
-                    val name = weekdayTurn[currentWeekdayIndex % size]
-                    yesterdayWorker = name
-                    currentWeekdayIndex++
-                    scheduleDay.worker = name
-                }
 
-                WorkType.HOLIDAY -> {
-                    var behind = 1
-                    val size = holidayTurn.size
-                    while (yesterdayWorker == holidayTurn[currentHolidayIndex % size]) {
-                        val tmp = holidayTurn[currentHolidayIndex % size]
-                        holidayTurn[currentHolidayIndex % size] = holidayTurn[(currentHolidayIndex + behind) % size]
-                        holidayTurn[(currentHolidayIndex + behind) % size] = tmp
-                        behind++
-                    }
-                    val name = holidayTurn[currentHolidayIndex % size]
-                    yesterdayWorker = name
-                    currentHolidayIndex++
-                    scheduleDay.worker = name
-                }
-            }
-            schedule.add(scheduleDay)
+        return calendar.map { day ->
+            val (worker, newWeekdayIndex, newHolidayIndex) = pickWorker(
+                day, weekdayTurn, holidayTurn, currentWeekdayIndex, currentHolidayIndex, yesterdayWorker
+            )
+            currentWeekdayIndex = newWeekdayIndex
+            currentHolidayIndex = newHolidayIndex
+            yesterdayWorker = worker
+            day.apply { this.worker = worker }
         }
-        return schedule
+    }
+
+    private fun pickWorker(
+        day: Day,
+        weekdayTurn: MutableList<String>,
+        holidayTurn: MutableList<String>,
+        weekdayIndex: Int,
+        holidayIndex: Int,
+        yesterdayWorker: String
+    ): Triple<String, Int, Int> {
+        return if (day.isHoliday) {
+            val worker = pickHolidayWorker(holidayTurn, holidayIndex, yesterdayWorker)
+            Triple(worker, weekdayIndex, holidayIndex + 1)
+        } else {
+            val worker = weekdayTurn[weekdayIndex % weekdayTurn.size]
+            Triple(worker, weekdayIndex + 1, holidayIndex)
+        }
+    }
+
+    private fun pickHolidayWorker(
+        holidayTurn: MutableList<String>,
+        index: Int,
+        yesterdayWorker: String
+    ): String {
+        val size = holidayTurn.size
+        var behind = 1
+
+        while (yesterdayWorker == holidayTurn[index % size]) {
+            swapWorkers(holidayTurn, index % size, (index + behind) % size)
+            behind++
+        }
+
+        return holidayTurn[index % size]
+    }
+
+    private fun swapWorkers(list: MutableList<String>, index1: Int, index2: Int) {
+        val tmp = list[index1]
+        list[index1] = list[index2]
+        list[index2] = tmp
     }
 }
