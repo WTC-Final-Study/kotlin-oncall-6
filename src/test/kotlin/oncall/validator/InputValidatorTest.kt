d092287579c6b8822d22b5e63846b16ac67446ef
joon0447 <joon0447@naver.com>
2025-12-19 12:25:21 +0900

    test: 순번 검증 테스트

--- src/test/kotlin/oncall/validator/InputValidatorTest.kt
+++ src/test/kotlin/oncall/validator/InputValidatorTest.kt
@@ -3,6 +3,8 @@ package oncall.validator
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputValidatorTest {
 
@@ -35,6 +37,38 @@ class InputValidatorTest {
         assertDoesNotThrow {
             val input = "12,금"
             InputValidator.validateMonthAndWeekday(input)
+        }
+    }
+
+    @Test
+    fun `닉네임이 5글자 초과하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val input = "수아,글로고니도밥,고니,도밥,준팍"
+            InputValidator.validateWorkTurn(input)
+        }
+    }
+
+    @Test
+    fun `중복된 닉네임을 입력하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val input = "수아,수아,글로,고니,도밥,준팍"
+            InputValidator.validateWorkTurn(input)
+        }
+    }
+
+    @Test
+    fun `5명 미만으로 입력하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val input = "수아,글로,도밥,준팍"
+            InputValidator.validateWorkTurn(input)
+        }
+    }
+
+    @Test
+    fun `정상 입력 시 예외가 발생하지 않는다`() {
+        assertDoesNotThrow {
+            val input = "수아,글로,도밥,준팍,고니"
+            InputValidator.validateWorkTurn(input)
         }
     }
 }
