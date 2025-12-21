3da98a19cbffb7f522cb7cb675aa4278ccc54e2a
joon0447 <joon0447@naver.com>
2025-12-21 14:13:58 +0900

    test: 스케줄 배정 테스트

---
+++ src/test/kotlin/oncall/service/ScheduleManagerTest.kt
@@ -0,0 +1,31 @@
+package oncall.service
+
+import oncall.model.StartInfo
+import oncall.model.WorkList
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class ScheduleManagerTest {
+
+    @Test
+    fun `스케줄 생성`() {
+        assertDoesNotThrow {
+            val startInfo = StartInfo(
+                month = 5,
+                dayOfWeek = 0
+            )
+
+            val calendarManager = CalendarManager()
+            calendarManager.generateCalendar(startInfo)
+            val calendar = calendarManager.getCalendar()
+            val workList = WorkList(
+                "준팍,도밥,고니,수아,루루,글로,솔로스타,우코,슬링키,참새,도리".split(",").toMutableList(),
+                "수아,루루,글로,솔로스타,우코,슬링키,참새,도리,준팍,도밥,고니".split(",").toMutableList()
+            )
+            ScheduleManager.generateSchedule(calendar, workList)
+            println(ScheduleManager.getSchedule().forEach { key, value ->
+                println("$key -> $value")
+            })
+        }
+    }
+}
