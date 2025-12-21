fd582a5a5685125f76e54eb73c2b757b2bec1dfb
joon0447 <joon0447@naver.com>
2025-12-21 13:10:34 +0900

    test: 달력 생성 테스트

---
+++ src/test/kotlin/oncall/service/CalendarManagerTest.kt
@@ -0,0 +1,28 @@
+package oncall.service
+
+import oncall.model.Day
+import oncall.model.StartInfo
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CalendarManagerTest {
+
+    @Test
+    fun `달력이 정상적으로 생성된다`() {
+        val startInfo = StartInfo(
+            month = 5,
+            dayOfWeek = 0
+        )
+        val calendarManager = CalendarManager()
+        calendarManager.generateCalendar(startInfo)
+        val calendar = calendarManager.getCalendar().get(5)
+        val childDay = Day(
+            day = 5,
+            isHoliday = true,
+            dayOfWeek = 4,
+            worker = ""
+        )
+        assertEquals(calendar, childDay)
+    }
+}
