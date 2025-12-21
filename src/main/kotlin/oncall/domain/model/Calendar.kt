76532743671dca56dcef283031a4f5ac1b7da4d6
baekcii <bsy8470@naver.com>
2025-12-21 14:19:53 +0900

    feat: 스케줄 배정 로직 구현

--- src/main/kotlin/oncall/domain/model/Calendar.kt
+++ src/main/kotlin/oncall/domain/model/Calendar.kt
@@ -1,27 +1,26 @@
 package oncall.domain.model
 
 class Calendar(
-    private val month: Int,
+    val month: Int,
     private val startDayOfWeek: String
 ) {
-    private val startDate = Date(month, 1)
-    private val lastDate: Date
+    val startDate = Date(month, 1)
+    val lengthOrMonth: Int
     private val dayOfWeek: List<String>
 
     init {
-        lastDate = calculateLastDate()
+        lengthOrMonth = calculateLengthOrMonth()
         dayOfWeek = calculateDayOfWeek()
     }
 
-    private fun calculateLastDate(): Date {
-        val date = when {
+    private fun calculateLengthOrMonth(): Int {
+        return when {
             month == 2 -> 28
             month < 8 && month % 2 == 1 -> 31
             month < 8 -> 30
             month >= 8 && month % 2 == 0 -> 31
             else -> 30
         }
-        return Date(month, date)
     }
 
     private fun calculateDayOfWeek(): List<String> {
@@ -33,13 +32,13 @@ class Calendar(
         return (0..6).map { dayOfWeekStr[(startIdx + it) % 7] }
     }
 
-    fun Date.dayOfWeek(): String {
-        val diff = startDate.date - this.date
+    fun dayOfWeek(date: Int): String {
+        val diff = startDate.date - date
         return dayOfWeek[diff % 7]
     }
 
-    fun Date.isWeekend(): Boolean {
-        val diff = startDate.date - this.date
+    fun onWeekend(date: Int): Boolean {
+        val diff = startDate.date - date
         val dayOfWeek = dayOfWeek[diff % 7]
         return dayOfWeek == "토" || dayOfWeek == "일"
     }
---
+++ src/main/kotlin/oncall/domain/model/Scheduler.kt
@@ -0,0 +1,62 @@
+package oncall.domain.model
+
+class Scheduler(
+    val calendar: Calendar
+) {
+
+    fun createSchedule(
+        weekdayOrder: List<String>,
+        weekendOrder: List<String>
+    ): List<DaySchedule> {
+        val schedule = arrange(weekdayOrder, weekendOrder)
+        return (schedule.indices).map { idx ->
+            DaySchedule(
+                month = calendar.month,
+                date = idx + 1,
+                dayOfWeek = calendar.dayOfWeek(idx + 1),
+                name = schedule[idx]
+            )
+        }
+    }
+
+    private fun arrange(weekdayOrder: List<String>, weekendOrder: List<String>): List<String> {
+        val weekdayQue = createQue(weekdayOrder)
+        val weekendQue = createQue(weekendOrder)
+        val schedule = mutableListOf<String>()
+        (1..calendar.lengthOrMonth).forEach { date ->
+            if (isHoliday(date) && canAssign(schedule, weekendQue.first())) {
+                schedule.add(weekendQue.removeFirst())
+            } else if (isHoliday(date)) {
+                val temp = weekendQue.removeFirst()
+                schedule.add(weekendQue.removeFirst())
+                weekendQue.addFirst(temp)
+            } else if (canAssign(schedule, weekdayQue.first())) {
+                schedule.add(weekdayQue.removeFirst())
+            } else {
+                val temp = weekdayQue.removeFirst()
+                schedule.add(weekdayQue.removeFirst())
+                weekdayQue.addFirst(temp)
+            }
+        }
+        return schedule
+    }
+
+    private fun createQue(order: List<String>): ArrayDeque<String> {
+        val que = ArrayDeque<String>()
+        val times = calendar.lengthOrMonth / order.size
+        repeat(times) {
+            que.addAll(order)
+        }
+        return que
+    }
+
+
+    fun canAssign(schedule: List<String>, name: String): Boolean {
+        return schedule.isEmpty() || schedule.last() != name
+    }
+
+    private fun isHoliday(date: Int): Boolean {
+        return calendar.onWeekend(date) || PublicHoliday.onHoliday(calendar.month, date)
+    }
+
+}
